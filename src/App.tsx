cc358f2212750e95dca1c3d772643845b6fb3cd0
Okarin-K <rachel9029@icloud.com>
2022-04-17 10:55:18 +0900

    add TODO機能
    追加機能
    チェックすると取り消し線を引く機能

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FormEvent, useState } from 'react';
 import './App.css';
 
+type Todo = {
+  id: number,
+  title: string,
+  completed: boolean
+};
+
 function App() {
+  const [title, setTitle] = useState('');
+
+  const [todoItems, setTodo] = useState<Todo[]>([]);
+
+  const addTodo = (value: string) => {
+    const todo = [...todoItems];
+    
+    todo.push({
+      id: 1,
+      title: value,
+      completed: false
+    });
+
+    setTodo(todo);
+  }
+
+  const updateTodo = (index: number) => {
+    const currentTodoItem = [...todoItems];
+
+    const target = currentTodoItem[index];
+    target.completed = !target.completed;
+
+    setTodo(currentTodoItem);
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    addTodo(title);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Please input...' />
+        <input type="submit" value="TODOを追加する" />
+      </form>
+      {todoItems.map((item, index) => {
+        if(item.title.length === 0) {
+          return;
+        }
+
+        return item.completed ?
+          <div key={index}><input type="checkbox" onChange={() => updateTodo(index)} /><label className='done'>{item.title}</label></div>:
+          <div key={index}><input type="checkbox" onChange={() => updateTodo(index)} /><label>{item.title}</label></div>;
+      })}
     </div>
   );
 }
