9b4f208b64c6e707b07693fc33f78d1b05df7ab0
Sazanamin <rachel9029@icloud.com>
2022-06-08 03:06:34 +0900

    delte frontソース

--- frontend/src/api/dataAccess/getTodoList.ts
+++
@@ -1,9 +0,0 @@
-export async function getTodoList(idToken: string) {
-    const response = await fetch('http://localhost:5000/todos', {
-        method: 'GET',
-        headers: {
-            'X-FBUser-Token': idToken,
-        },
-    });
-    return response.json();
-}
--- frontend/src/types/todo.ts
+++
@@ -1,7 +0,0 @@
-import { Timestamp } from 'firebase/firestore/lite';
-
-export type Todo = {
-    title: string;
-    completed: boolean;
-    created_at: Timestamp;
-};
